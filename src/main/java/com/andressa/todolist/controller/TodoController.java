3c9893c62032acad74cb6d61789eebc84f671b8e
andressa-ricardo <andressaricardo30@gmail.com>
2023-11-23 18:02:49 -0300

    creation methods post, get, put and delete

---
+++ src/main/java/com/andressa/todolist/controller/TodoController.java
@@ -0,0 +1,61 @@
+package com.andressa.todolist.controller;
+
+import com.andressa.todolist.entities.Todo;
+import com.andressa.todolist.repository.TodoRepository;
+
+import jakarta.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping()
+public class TodoController {
+
+    private final TodoRepository todoRepository;
+
+    @Autowired
+    public TodoController(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    @GetMapping("/tarefas")
+    public ResponseEntity<List<Todo>> getAllTarefas() {
+        List<Todo> todos = todoRepository.findAll();
+        return ResponseEntity.ok(todos);
+    }
+   
+
+    @PostMapping("/tarefa")
+    public ResponseEntity<Todo> createTodo(@Valid @RequestBody Todo todo) {
+        Todo savedTodo = todoRepository.save(todo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedTodo);
+    }
+
+    @PutMapping("/tarefa/{id}")
+    public ResponseEntity<Todo> updateTodo(
+            @PathVariable UUID id,
+            @Valid @RequestBody Todo updatedTodo
+    ) {
+        if (!todoRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        updatedTodo.setId(id);
+        Todo savedTodo = todoRepository.save(updatedTodo);
+        return ResponseEntity.ok(savedTodo);
+    }
+
+    @DeleteMapping("/tarefa/{id}")
+    public ResponseEntity<Void> deleteTodoById(@PathVariable UUID id) {
+        if (!todoRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        todoRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}

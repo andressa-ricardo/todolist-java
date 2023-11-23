af32c0adef4c76f6625fb757fb166c0fb8550b41
andressa-ricardo <andressaricardo30@gmail.com>
2023-11-23 16:48:53 -0300

    entity created

--- src/main/java/com/andressa/todolist/entities/Todo.java
+++ src/main/java/com/andressa/todolist/entities/Todo.java
@@ -1,6 +1,45 @@
 package com.andressa.todolist.entities;
 
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.andressa.todolist.enums.EnumPriority;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+
+@RestController
+@Entity
+@Table(name = "tarefas")
 
 public class Todo {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column (unique = false, nullable = false, length = 50)
+    private String tarefa;
+
+    @NotEmpty
+    private String description;
+
+    @NotEmpty
+    private EnumPriority priority;
+
+    public Todo(){
+        
+    }
+
+    public Todo(String tarefa, @NotEmpty String description, @NotEmpty EnumPriority priority) {
+        this.tarefa = tarefa;
+        this.description = description;
+        this.priority = priority;
+    }
+
 }
